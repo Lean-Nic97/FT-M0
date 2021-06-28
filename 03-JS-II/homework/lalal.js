da0c990c50f7a9556f8e3e30b6615afa9462fe45
Lean-Nic97 <leapark_lp@hotmail.com>
2021-06-28 16:27:25 -0300

    mi primer commit

---
+++ 03-JS-II/homework/lalal.js
@@ -0,0 +1,39 @@
+
+// console.log(1 === '1')
+
+{/* < > <= >= === == !==  Quokka.js */}
+
+// AND --> &&
+
+// OR --> ||
+
+// NOT --> !
+
+/*
+    1E    2E     &&      ||
+    true  true  true    true
+    true  false  false   true
+    false true  false   true
+    false false false   false
+*/
+
+// = == ===
+
+function mostrarNumeros(numeroLimite) {
+    // for (var i = numeroLimite; i >= 1; i-- ) {
+    //    for (var j = 0; j < numeroLimite; j++) {
+    //        console.log(i * j);
+    //    }
+    // };
+    var i = 1;
+    // var seguir = true;
+    do {
+        console.log(i);
+    } while (i < 1);
+
+    console.log("Se termino el bucle/loop/ciclo");
+};
+
+mostrarNumeros(30);
+
+
